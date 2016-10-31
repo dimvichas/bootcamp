837555586086ec4baabb5024da65ff1e10a8e5fe
dimvichas <dim.vihas@gmail.com>
2016-11-01 00:42:40 +0200

    Add files via upload

---
+++ C-week1/demo1/demo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Give me a number between 1 and 6: ");
+            string NumberStr = Console.ReadLine();
+            int Number = Int32.Parse(NumberStr);
+            if (Number >= 6)
+            {
+                Console.WriteLine("Not valide number");
+            }
+            else
+            {
+
+                Random r = new Random();
+                int LotteryNumber = r.Next(1, 7);
+                if (Number == LotteryNumber)
+                {
+                    Console.WriteLine("You win!");
+                }
+                else
+                {
+                    Console.WriteLine("You loose \nThe winning number is " + LotteryNumber);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
